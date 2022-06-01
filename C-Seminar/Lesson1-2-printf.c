2f3a0ac722c71dca36c7f89a3e369918d0ee8f80
haim hod gilboa <haim@newclass.co.il>
2022-06-01 18:34:36 +0300

    seccond lesson

--- C-Seminar/Lesson1-2-printf.c
+++ C-Seminar/Lesson1-2-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #pragma warning(disable : 4996)
 
-int main()
+int main_1_2()
 {
     printf("This is number %d and another number %d \n", 13, 112);
     printf(" number %d \n+\n number %d  = %d \n", 13, 12, 12+13);
@@ -9,8 +9,3 @@ int main()
     return 0;
 }
 
-
-
-************
-*          *
-************
