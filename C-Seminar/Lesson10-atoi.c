02dda6c70593109a83f5c1d0e3c002824e8a888f
haimnewclass <haim@newclass.co.il>
2022-07-14 20:09:08 +0300

    1

--- C-Seminar/Lesson10-atoi.c
+++ C-Seminar/Lesson10-atoi.c
@@ -7,8 +7,7 @@ int main_10_atio()
 	char str[1000];
 	
 	gets(str);
-	
-
+				
 
 	char str1[100];
 	strcpy(str1, "100");
@@ -21,6 +20,6 @@ int main_10_atio()
 	num1 = atoi(str1);
 	num2 = atoi(str2);
 	sum = num1 + num2;
-
+	printf("");
 	return 0;
 }
--- C-Seminar/Lesson9-pointer-strstr2.c
+++ C-Seminar/Lesson9-pointer-strstr2.c
@@ -1,22 +1,45 @@
 #include <stdio.h>
 #include <string.h>
+#include <limits.h>
 #pragma warning(disable : 4996)
 
-int main_9_90()
+int main()
 {
 
 	char str1[1000];
 	char str2[1000];
-
-	gets(str1);
+	unsigned char numCars=89;
+	
+	gets(str1); //"1234567890123456789"
 	gets(str2);
+	char* m= str1;
+	char* ptr = str1;
+	printf("%s\n", m);
 
-	//
-	char* p = str1;
-	p = p + 3;
+	m = m + 5;
+
+	printf("%s\n", m);
 
 
-	//
+	for (int i = 0; i < 8; i++)
+	{
+		m--;
+	}
+	printf("%s\n", m);
+
+	
+	int a,b;
+	scanf("%d %d", &a, &b);
+
+	m = m - 3;
+	m = a * b;
+	printf("%s\n", m);
+
+	for (int i = 0; i < strlen(str1); i=i+2)
+	{
+		printf("%s\n", m);
+	}
+
 
 	char* pt = strstr(str2, str1);
 	if (pt != 0)
